6834cdca1af8c62394506ac48a7734a8760f3669
Aliguliev <ealiguliev@gmail.com>
2025-11-04 11:34:30 +0300

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ hamburger.addEventListener('click', () => {
     navMenu.classList.toggle('active');
 });
 cv.addEventListener('click', () => {
-    document.location.href='/Резюме.docx';
+    document.location.href='Резюме.docx';
 });
 document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', () => {
     hamburger.classList.remove('active');
@@ -38,3 +38,4 @@ window.addEventListener('scroll', () => {
     }
 
 });
+
